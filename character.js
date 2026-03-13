7b2a411519a44a9e4ecd3b37eae3610c51fee4f8
Žiga Makuc <ziga.makuc.7@gmail.com>
2026-03-13 09:52:05 +0100

    Refactor character.js to improve structure and clarity

--- character.js
+++ character.js
@@ -308,8 +308,8 @@ resize();
 rebuildMask();
 
 const r = canvas.getBoundingClientRect();
-player.x = r.width * 0.55;
-player.y = r.height * 0.05;
+player.x = r.width * 0.50;
+player.y = r.height * 0.00;
 
 {
   const { dw, dh } = spriteSize();
@@ -325,3 +325,4 @@ player.y = r.height * 0.05;
 }
 
 requestAnimationFrame(loop);
+
